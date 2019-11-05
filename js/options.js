a470fa86a106019f5ec2a25994ce5517d65eb98d
LaloAntonio <55796340+LaloAntonio@users.noreply.github.com>
2019-11-05 16:03:43 -0600

    changes from 05112010

--- js/options.js
+++ js/options.js
@@ -19,17 +19,39 @@ var myDropzone = new Dropzone(".dropzone",{
         return year+'-'+month+'-'+date+'-'+hour+'-'+minute+'-'+sec+'-' + msec +'_file.jpg';
     },
     init: function() {
-        myDropzone = this;
         $('#uploadfiles').click(function(){
-            myDropzone.processQueue();
+            swal({
+                title: "Desea Registrar esta Noticia?",
+                icon: "info",
+                buttons: true,
+                dangerMode: true,
+            })
+            .then((willDelete) => {
+                if (willDelete) {
+                    if(myDropzone.getQueuedFiles().length == 0 && myDropzone.getUploadingFiles().length == 0 ){
+                        swal("No hay imagenes agregadas al DropZone");
+                    }else {
+                        myDropzone.processQueue();
+                        swal("Noticia Agregada", {
+                            icon: "success",
+                        });
+                    }
+                }
+            });
+            
         });
-        this.on("complete", function(){
-            if(this.getQueuedFiles().length == 0 && this.getUploadingFiles().length == 0) {
-                var _this = this;
-                _this.removeAllFiles();
-            }
+        this.on("sendingmultiple", function(data, xhr, formData) {
+            var ckdata = CKEDITOR.instances['editor'].getData();
+            formData.append('name', jQuery('#name').val());
+            formData.append('subname', jQuery('#subname').val());
+            formData.append('editor', ckdata);
+        });
+        this.on("complete", function(file){
+            myDropzone.removeFile(file);
+            $("#name").val('');
+            $("#subname").val('');
+            CKEDITOR.instances['editor'].setData('');
         });
     }
-    });
- 
-    
+});
+
--- upload.php
+++ upload.php
@@ -3,13 +3,22 @@ date_default_timezone_set("America/Mexico_City");
 include 'connecdb.php';
 $location = 'upload/';
 $date = date("Y-m-d-h-i-s");
+$name = $_POST['name'];
+$subname = $_POST['subname'];
+$editor = $_POST['editor'];
+$insert2 = "INSERT INTO noticia VALUE ('','$date','$name','$subname','$editor')";
+$query2 = mysqli_query($mysqli,$insert2);
+$last_id = mysqli_insert_id($mysqli);
+if ($query2) {
+	echo "<br>Noticia Agregada";
+}
 if (isset($_FILES['file']) && $_FILES['file']['name'][0]) {
 	for ($i=0;$i<count($_FILES['file']['name']);$i++) { 
 		$origen=$_FILES["file"]["tmp_name"][$i];
 		$namefile=$_FILES["file"]["name"][$i];
 		$destino=$location.$namefile;
 		if (@move_uploaded_file($origen, $destino)) {
-			$insert = "INSERT INTO imagenoticia VALUE ('','$namefile','$date')";
+			$insert = "INSERT INTO imagenoticia VALUE ('','$namefile','$date','$last_id')";
 			$query = mysqli_query($mysqli,$insert);
 			if ($query) {
 				echo "<br> Dato ingresado";
@@ -21,4 +30,5 @@ if (isset($_FILES['file']) && $_FILES['file']['name'][0]) {
 }else {
 	echo "<br>No se ha subido ninguna imagen";
 }
+
 ?>
